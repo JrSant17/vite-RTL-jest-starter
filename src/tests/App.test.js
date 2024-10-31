04612b88670fec0bcf98f2f8adc05e7bafb8c2b0
santiagojoejr <santiagojoejr@gmail.com>
2024-10-31 13:55:08 -0700

    first commit

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,28 +1,20 @@
 import { render, screen, fireEvent } from "@testing-library/react";
-import App from "../App";
+import App from "../App.jsx";
 import React from "react";
 
-describe(`App`, () => {
-  beforeEach(() => {
-    // Pre-Arrange
-    render(<App />);
-  });
+test('As a Chef, I want to store my receipes so that I can keep track of them.', () => {
+  //1. Render the landing page
+  render(<App/>)
+  //2. Wait for the page to load
+  // Implied in this case, nothing to wait for
 
-  it(`initializes with text 'Vite + React'`, () => {
-    // Act
-    // Assert
-    expect(screen.getByRole("heading")).toHaveTextContent("Vite + React");
-  });
+  //3. Then I should see a heading that reads "My Recipes"
+  let recipeHeader = screen.getByText('My Recipes');
+  expect(recipeHeader).toBeInTheDocument();
+  
+  //4. And I should see text beneath the heading that reads "There are no recipes to list"
+  let recipeList = screen.getByText('There are no recipes to list.');
+  expect(recipeList).toBeInTheDocument();
 
-  it(`increments counter on button click`, () => {
-    // Arrange
-    const button = screen.getByRole("button");
-    const counterText = screen.getByText(/Count is: /i);
-
-    expect(counterText).toHaveTextContent("Count is: 0");
-    // Act
-    fireEvent.click(button);
-    // Assert
-    expect(counterText).toHaveTextContent("Count is: 1");
-  });
 });
+
